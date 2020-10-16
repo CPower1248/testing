cf03118bb5b0cf858b5aceb109024ddb3be3ccd2
corey-power <CPower1248@gmail.com>
2020-10-16 19:41:58 -0400

    Install React and Cypress, need React Router

---
+++ marvel-api/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+export default function App() {
+  // Initializers
+  const initialFormValues = {
+    name: "",
+
+  }
+
+  return (
+    <div className="App">
+      <span>--testing--</span>
+      <h1>Marvel API</h1>
+    </div>
+  );
+}
