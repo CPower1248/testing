7a472074a3d1aad4b9e699d4092219081b11f17d
corey-power <CPower1248@gmail.com>
2020-10-16 20:06:41 -0400

    Swapping between Home/Form with React Router

--- marvel-api/src/App.js
+++ marvel-api/src/App.js
@@ -1,17 +1,30 @@
 import React from 'react';
+import { Switch, Route, Link } from "react-router-dom"
 import './App.css';
+import Form from "./Components/Form"
 
 export default function App() {
   // Initializers
-  const initialFormValues = {
-    name: "",
-
-  }
 
   return (
     <div className="App">
-      <span>--testing--</span>
-      <h1>Marvel API</h1>
+
+      <Link to="/">Home</Link>
+      <Link to="/Form">Form</Link>
+      <div>
+        <span>--testing--</span>
+        <h1>Marvel API</h1>
+      </div>
+
+      <Switch>
+        <Route path="/Form">
+          <Form />
+        </Route>
+
+        <Route path="/">
+          <h2>Home</h2>
+        </Route>
+      </Switch>
     </div>
   );
 }
